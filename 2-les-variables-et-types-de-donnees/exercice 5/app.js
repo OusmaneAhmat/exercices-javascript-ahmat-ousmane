570619eeee0fcbbf14814026dc5eb9f987799485
OusmaneAhmat <aousmane@deastanceservices.fr>
2024-03-07 10:22:21 +0100

    exo 6

--- 2-les-variables-et-types-de-donnees/exercice 5/app.js
+++ 2-les-variables-et-types-de-donnees/exercice 5/app.js
@@ -1,11 +1,11 @@
 let age = 25;
-console.log(typeof 25);
+console.log(typeof age);
 
 let genre = "femme";
-console.log(typeof "femme");
+console.log(typeof genre);
 
 let estContent = true;
-console.log(typeof true);
+console.log(typeof estContent);
 
 
 
---
+++ 2-les-variables-et-types-de-donnees/exercice 6/main.js
@@ -0,0 +1,12 @@
+
+let age = prompt("Quel est ton age");
+
+console.log(typeof age)
+
+age++
+console.log(age); 
+
+
+
+
+
