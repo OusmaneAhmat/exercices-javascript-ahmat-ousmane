7294d40b329e6b1747273cf65cb8417958dacb70
OusmaneAhmat <aousmane@deastanceservices.fr>
2024-03-07 10:53:11 +0100

    toujours les exo sur les variables

---
+++ 2-les-variables-et-types-de-donnees/exercice 7/app.js
@@ -0,0 +1,15 @@
+let age = prompt("Quel est ton age");
+let prix = prompt("combien coute une banane")
+
+
+
+
+
+
+// Résultat attendu pour 10 et 2, dans la console :
+
+10 x 2 = 20
+10 - 2 = 8
+10 + 2 = 12
+10 / 2 = 5
+10 exposant 2 = 100
